118e5747c2bbba71645fe3d0a52dd1b38436b72f
RyanRongY <yarong@ucsd.edu>
2023-03-01 13:38:05 -0800

    part 2

--- customdialog.js
+++ customdialog.js
@@ -1,37 +1,94 @@
-// create the dialog element
-const dialog = document.createElement("dialog");
+import DOMPurify from 'https://cdn.jsdelivr.net/npm/dompurify@2.3.1/dist/purify.min.js';
 
-// create the dialog content
-const dialogContent = `
-  <form>
-    <label for="inputField">Enter your message:</label>
-    <br>
-    <input type="text" id="inputField">
-    <br><br>
-    <button type="submit">OK</button>
-    <button type="button" onclick="dialog.close()">Cancel</button>
-  </form>
-`;
+// Get dialog elements
+const alertDialog = document.querySelector('#alert-dialog');
+const confirmDialog = document.querySelector('#confirm-dialog');
+const promptDialog = document.querySelector('#prompt-dialog');
+const saferPromptDialog = document.querySelector('#safer-prompt-dialog');
 
-// add the content to the dialog element
-dialog.innerHTML = dialogContent;
+// Get dialog elements for manipulation
+const alertDialogText = alertDialog.querySelector('p');
+const confirmDialogText = confirmDialog.querySelector('p');
+const promptInput = promptDialog.querySelector('#prompt-input');
+const saferPromptInput = saferPromptDialog.querySelector('#safer-prompt-input');
+const confirmButton = confirmDialog.querySelector('.confirm-button');
+const cancelButton = confirmDialog.querySelector('.cancel-button');
+const dialogCloseButtons = document.querySelectorAll('.close-button');
 
-// add the dialog element to the document body
-document.body.appendChild(dialog);
-
-// function to open the dialog and handle the form submission
-function showDialog() {
-  // open the dialog
-  dialog.showModal();
-
-  // handle the form submission
-  const form = dialog.querySelector("form");
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const inputField = form.querySelector("#inputField");
-    const input = inputField.value;
-    const sanitizedInput = DOMPurify.sanitize(input);
-    document.getElementById("output").innerHTML = `Your message is: ${sanitizedInput}`;
-    dialog.close();
-  });
+// Close all dialogs function
+function closeAllDialogs() {
+  alertDialog.close();
+  confirmDialog.close();
+  promptDialog.close();
+  saferPromptDialog.close();
 }
+
+// Alert function
+function customAlert(message) {
+  alertDialogText.innerHTML = DOMPurify.sanitize(message);
+  alertDialog.showModal();
+}
+
+// Confirm function
+function customConfirm(message, callback) {
+  confirmDialogText.innerHTML = DOMPurify.sanitize(message);
+  confirmButton.addEventListener('click', () => {
+    callback(true);
+    closeAllDialogs();
+  });
+  cancelButton.addEventListener('click', () => {
+    callback(false);
+    closeAllDialogs();
+  });
+  confirmDialog.showModal();
+}
+
+// Prompt function
+function customPrompt(message, callback) {
+  promptInput.value = '';
+  promptInput.focus();
+  const onSubmit = () => {
+    const value = DOMPurify.sanitize(promptInput.value);
+    if (value) {
+      callback(value);
+      closeAllDialogs();
+    }
+  };
+  promptInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      onSubmit();
+    }
+  });
+  confirmButton.addEventListener('click', onSubmit);
+  cancelButton.addEventListener('click', closeAllDialogs);
+  promptDialog.showModal();
+}
+
+// Safer prompt function
+function customSaferPrompt(message, callback) {
+  saferPromptInput.value = '';
+  saferPromptInput.focus();
+  const onSubmit = () => {
+    const value = DOMPurify.sanitize(saferPromptInput.value);
+    if (value) {
+      callback(value);
+      closeAllDialogs();
+    }
+  };
+  saferPromptInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      onSubmit();
+    }
+  });
+  confirmButton.addEventListener('click', onSubmit);
+  cancelButton.addEventListener('click', closeAllDialogs);
+  saferPromptDialog.showModal();
+}
+
+// Attach event listeners to close dialog buttons
+for (const closeButton of dialogCloseButtons) {
+  closeButton.addEventListener('click', closeAllDialogs);
+}
+
+// Export custom dialog functions
+export { customAlert, customConfirm, customPrompt, customSaferPrompt };
