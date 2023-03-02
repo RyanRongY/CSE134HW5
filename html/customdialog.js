956dee26bdff42e8d36789c22e593db9fcda11bb
RyanRongY <yarong@ucsd.edu>
2023-03-01 16:54:22 -0800

    part 1

--- html/customdialog.js
+++ html/customdialog.js
@@ -1,44 +1,31 @@
-const dialogTemplate = `
-  <dialog>
-    <form method="dialog">
-      <h1></h1>
-      <p></p>
-      <input type="text" />
-      <button type="submit"></button>
-      <button type="reset"></button>
-    </form>
-  </dialog>
-`;
-
-export function showDialog(title, message, submitText, cancelText) {
-  // Create a new dialog element from the template
-  const dialog = document.createElement('template');
-  dialog.innerHTML = DOMPurify.sanitize(dialogTemplate);
-  const dialogElement = dialog.content.firstElementChild;
-
-  // Set the title and message of the dialog
-  dialogElement.querySelector('h1').textContent = title;
-  dialogElement.querySelector('p').textContent = message;
-
-  // Set the text of the submit and cancel buttons
-  dialogElement.querySelector('button[type="submit"]').textContent = submitText;
-  dialogElement.querySelector('button[type="reset"]').textContent = cancelText;
-
-  // Show the dialog
-  document.body.append(dialogElement);
-  dialogElement.showModal();
-
-  // Return a Promise that resolves with the user input or null
-  return new Promise((resolve, reject) => {
-    dialogElement.querySelector('form').addEventListener('submit', (event) => {
-      event.preventDefault();
-      const input = dialogElement.querySelector('input').value;
-      dialogElement.remove();
-      resolve(input);
-    });
-    dialogElement.querySelector('form').addEventListener('reset', () => {
-      dialogElement.remove();
-      resolve(null);
-    });
-  });
-}
+function showAlert() {
+	window.alert('This is an alert message!');
+  }
+  
+  function showConfirm() {
+	const result = window.confirm('Do you confirm?');
+	const confirmOutput = document.querySelector('#confirm-output');
+	if (confirmOutput) {
+	  confirmOutput.textContent = `The value returned by the confirm method is: ${result}`;
+	}
+  }
+  
+  function showPrompt() {
+	const input = window.prompt('Please enter something:');
+	if (input) {
+	  window.alert(`You entered: ${input}`);
+	} else {
+	  window.alert('User didn’t enter anything');
+	}
+  }
+  
+  function showSaferPrompt() {
+	const input = window.prompt('Please enter something:');
+	const safeInput = DOMPurify.sanitize(input);
+	if (safeInput) {
+	  window.alert(`You entered: ${safeInput}`);
+	} else {
+	  window.alert('User didn’t enter anything');
+	}
+  }
+  
