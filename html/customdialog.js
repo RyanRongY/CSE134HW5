22c1fef7ce1e657baf41ab0ced1acb4b365ef65d
RyanRongY <yarong@ucsd.edu>
2023-03-01 17:49:09 -0800

    part 2

--- html/customdialog.js
+++ html/customdialog.js
@@ -1,103 +1,77 @@
+const alertDialog = document.getElementById("alertDialog");
+const alertOkBtn = document.getElementById("alertOkBtn");
+const confirmDialog = document.getElementById("confirmDialog");
+const confirmYesBtn = document.getElementById("confirmYesBtn");
+const confirmNoBtn = document.getElementById("confirmNoBtn");
+const promptDialog = document.getElementById("promptDialog");
+const promptInput = document.getElementById("promptInput");
+const promptOkBtn = document.getElementById("promptOkBtn");
+const promptCancelBtn = document.getElementById("promptCancelBtn");
+const saferPromptDialog = document.getElementById("saferPromptDialog");
+const saferPromptInput = document.getElementById("saferPromptInput");
+const saferPromptOkBtn = document.getElementById("saferPromptOkBtn");
+const saferPromptCancelBtn = document.getElementById("saferPromptCancelBtn");
+const confirmOutput = document.getElementById("confirmOutput");
 
+alertBtn.addEventListener("click", () => {
+  alertDialog.showModal();
+});
 
-const output = document.getElementById('outputField');
+alertOkBtn.addEventListener("click", () => {
+  alertDialog.close();
+});
 
-// Alert dialog
-function createAlertDialog() {
-  const alertTemplate = document.getElementById('alertTemplate');
-  document.body.appendChild(alertTemplate.content.cloneNode(true));
-  const alertDialog = document.querySelector('dialog');
-  const alertMessage = document.getElementById('alertMessage');
-  const alertYesButton = document.getElementById('alertYesButton');
-  alertBtn.addEventListener('click', () => {
-    alertMessage.textContent = 'This is an alert dialog';
-    alertDialog.showModal();
-  });
-  alertYesButton.addEventListener('click', () => {
-    alertDialog.close();
-  });
-}
+confirmBtn.addEventListener("click", () => {
+  confirmDialog.showModal();
+});
 
-// Confirm dialog
-function createConfirmDialog() {
-  const confirmTemplate = document.getElementById('confirmTemplate');
-  document.body.appendChild(confirmTemplate.content.cloneNode(true));
-  const confirmDialog = document.querySelector('dialog');
-  const confirmMessage = document.getElementById('confirmMessage');
-  const confirmYesButton = document.getElementById('confirmYesButton');
-  const confirmNoButton = document.getElementById('confirmNoButton');
-  confirmBtn.addEventListener('click', () => {
-    confirmMessage.textContent = 'This is a confirm dialog';
-    confirmDialog.showModal();
-  });
-  confirmYesButton.addEventListener('click', () => {
-    confirmDialog.close();
-    output.textContent = 'The value returned by the confirm method is: true';
-  });
-  confirmNoButton.addEventListener('click', () => {
-    confirmDialog.close();
-    output.textContent = 'The value returned by the confirm method is: false';
-  });
-}
+confirmYesBtn.addEventListener("click", () => {
+  confirmOutput.textContent = "The value returned by the confirm method is: true";
+  confirmDialog.close();
+});
 
-// Prompt dialog
-function createPromptDialog() {
-  const promptTemplate = document.getElementById('promptTemplate');
-  document.body.appendChild(promptTemplate.content.cloneNode(true));
-  const promptDialog = document.querySelector('dialog');
-  const promptMessage = document.getElementById('promptMessage');
-  const promptInput = document.getElementById('promptInput');
-  const promptCancelButton = document.getElementById('promptCancelButton');
-  const promptYesButton = document.getElementById('promptYesButton');
-  promptBtn.addEventListener('click', () => {
-    promptMessage.textContent = 'Please enter your name:';
-    promptInput.value = '';
-    promptDialog.showModal();
-  });
-  promptCancelButton.addEventListener('click', () => {
-    promptDialog.close();
-    output.textContent = 'User cancelled the prompt';
-  });
-  promptYesButton.addEventListener('click', () => {
-    const promptValue = promptInput.value.trim();
-    if (promptValue === '') {
-      output.textContent = 'User didn\'t enter anything';
-    } else {
-      output.textContent = `Your name is ${promptValue}`;
-    }
-    promptDialog.close();
-  });
-}
+confirmNoBtn.addEventListener("click", () => {
+  confirmOutput.textContent = "The value returned by the confirm method is: false";
+  confirmDialog.close();
+});
 
-// Custom dialog
-function createCustomDialog() {
-  const customTemplate = document.getElementById('customTemplate');
-  document.body.appendChild(customTemplate.content.cloneNode(true));
-  const customDialog = document.querySelector('dialog');
-  const customTitle = document.getElementById('customTitle');
-  const customMessage = document.getElementById('customMessage');
-  const customYesButton = document.getElementById('customYesButton');
-  const customNoButton = document.getElementById('customNoButton');
-  customBtn.addEventListener('click', () => {
-    customTitle.textContent = 'Custom Dialog';
-    customMessage.textContent = 'Do you want to proceed with this action?';
-    customDialog.showModal();
-  });
-  customYesButton.addEventListener('click', () => {
-    customDialog.close();
-    output.textContent = 'User clicked Yes';
-  });
-  customNoButton.addEventListener('click', () => {
-    customDialog.close();
-    output.textContent = 'User clicked No';
-  });
-}
+promptBtn.addEventListener("click", () => {
+  promptDialog.showModal();
+});
 
-// Call dialog functions
-createAlertDialog();
-createConfirmDialog();
-createPromptDialog();
-createCustomDialog();
+promptOkBtn.addEventListener("click", () => {
+  if (promptInput.value === "") {
+    console.log("User didn't enter anything");
+  } else {
+    console.log(`Hello, ${promptInput.value}!`);
+  }
+  promptInput.value = "";
+  promptDialog.close();
+});
 
-// Sanitize input
-DOMPurify.add
+promptCancelBtn.addEventListener("click", () => {
+  console.log("User cancelled the prompt");
+  promptInput.value = "";
+  promptDialog.close();
+});
+
+saferPromptBtn.addEventListener("click", () => {
+  saferPromptDialog.showModal();
+});
+
+saferPromptOkBtn.addEventListener("click", () => {
+  const sanitizedResult = DOMPurify.sanitize(saferPromptInput.value);
+  if (sanitizedResult === "") {
+    console.log("User didn't enter anything");
+  } else {
+    console.log(`Hello, ${sanitizedResult}!`);
+  }
+  saferPromptInput.value = "";
+  saferPromptDialog.close();
+});
+
+saferPromptCancelBtn.addEventListener("click", () => {
+  console.log("User cancelled the prompt");
+  saferPromptInput.value = "";
+  saferPromptDialog.close();
+});
