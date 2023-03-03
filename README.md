a966913f95a5ee5e8f8ab9b7beb959d787324d05
RyanRongY <yarong@ucsd.edu>
2023-03-02 18:32:59 -0800

    ss

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CSE134B portfolio
 # Yan Rong's homework A16385775
+
+For HW4 
+the newly launched website is at https://imaginative-sable-246f14.netlify.app/
+
+
+For part 1 and part 2 I created 2 html files so that it will be easier
+to see the native dialogs and custom dialogs used on a webpage 
+so customdialog.js will also have a corresponding customdialog.html 
+
+For part 4 the css file is newly created called stylecrud.css
--- html/blog.js
+++ html/blog.js
@@ -1,66 +1,119 @@
-// Import DOMPurify
-import DOMPurify from 'https://cdn.skypack.dev/dompurify@2.3.1';
+// Define the main module
+const Blog = (() => {
+  // Private variables and functions
+  let posts = [];
 
-// Define the post template
-const postTemplate = document.querySelector('#post-template');
-
-// Define the posts array and load from localStorage if available
-let posts = [];
-
-const savedPosts = localStorage.getItem('posts');
-if (savedPosts) {
-  posts = JSON.parse(savedPosts);
-}
-
-// Function to save the posts array to localStorage
-function savePosts() {
-  localStorage.setItem('posts', JSON.stringify(posts));
-}
-
-// Function to render the posts in the UI
-function renderPosts() {
-  const postsContainer = document.querySelector('#posts');
-  postsContainer.innerHTML = '';
-  posts.forEach((post) => {
-    const postElement = postTemplate.content.cloneNode(true);
-    postElement.querySelector('h3').textContent = post.title;
-    postElement.querySelector('p:nth-of-type(1)').textContent = post.date;
-    postElement.querySelector('p:nth-of-type(2)').textContent = post.summary;
-    postsContainer.appendChild(postElement);
-  });
-}
-
-// Function to add a new post
-function addPost(title, date, summary) {
-  const newPost = {
-    id: Date.now().toString(),
-    title,
-    date,
-    summary
+  const savePosts = () => {
+    // Save the posts array to localStorage
+    localStorage.setItem('blogPosts', JSON.stringify(posts));
   };
-  posts.push(newPost);
-  savePosts();
-  renderPosts();
-}
 
-// Function to initialize the app
-function init() {
-  // Render the existing posts in the UI
-  renderPosts();
-
-  // Attach event listener to the Add Post form
-  const addPostForm = document.querySelector('form');
-  addPostForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const title = addPostForm.querySelector('#title').value.trim();
-    const date = addPostForm.querySelector('#date').value;
-    const summary = DOMPurify.sanitize(addPostForm.querySelector('#summary').value.trim());
-    if (title && date && summary) {
-      addPost(title, date, summary);
-      addPostForm.reset();
+  const loadPosts = () => {
+    // Load the posts array from localStorage
+    const savedPosts = localStorage.getItem('blogPosts');
+    if (savedPosts) {
+      posts = JSON.parse(savedPosts);
     }
-  });
-}
+  };
 
-// Call the init function to initialize the app
-init();
+  const renderPost = (post) => {
+    // Clone the post template
+    const template = document.getElementById('post-template');
+    const postMarkup = template.content.cloneNode(true);
+    // Update the post markup with the post data
+    const postTitle = postMarkup.querySelector('.post-title');
+    const postDate = postMarkup.querySelector('.post-date');
+    const postSummary = postMarkup.querySelector('.post-summary');
+    postTitle.textContent = post.title;
+    postDate.textContent = post.date;
+    postSummary.textContent = post.summary;
+    // Attach event listeners to the edit and delete buttons
+    const editButton = postMarkup.querySelector('.edit-post');
+    const deleteButton = postMarkup.querySelector('.delete-post');
+    editButton.addEventListener('click', () => {
+      editPost(post);
+    });
+    deleteButton.addEventListener('click', () => {
+      deletePost(post);
+    });
+    // Return the updated post markup
+    return postMarkup;
+  };
+
+  const renderPosts = () => {
+    // Get the DOM element where the posts will be displayed
+    const postList = document.getElementById('post-list');
+    // Clear the existing content
+    postList.innerHTML = '';
+    // Loop over the posts array and render each post
+    posts.forEach((post) => {
+      const postMarkup = renderPost(post);
+      postList.appendChild(postMarkup);
+    });
+  };
+
+  const addPost = (post) => {
+    // Add a new post to the posts array
+    posts.push(post);
+    // Render the updated list of posts
+    renderPosts();
+    // Save the posts array to localStorage
+    savePosts();
+  };
+
+  const deletePost = (post) => {
+    // Find the index of the post in the posts array
+    const index = posts.indexOf(post);
+    // Remove the post from the posts array
+    posts.splice(index, 1);
+    // Render the updated list of posts
+    renderPosts();
+    // Save the posts array to localStorage
+    savePosts();
+  };
+
+  const editPost = (post) => {
+      const { title, date, summary } = post;
+      const newTitle = prompt('Edit title:', title);
+      const newDate = prompt('Edit date:', date);
+      const newSummary = prompt('Edit summary:', summary);
+      if (newTitle && newDate && newSummary) {
+        post.title = newTitle;
+        post.date = newDate;
+        post.summary = newSummary;
+        renderPosts();
+        savePosts();
+      }
+    };
+    
+    
+    
+    
+  // Public API
+  return {
+    init() {
+      // Load any saved posts from localStorage
+      loadPosts();
+      // Render the initial set of posts
+      renderPosts();
+      // Attach event listener to the form submission
+      const addForm = document.getElementById('add-form');
+      addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const formData = new FormData(addForm);
+        const newPost = {
+          title: formData.get('title'),
+          date: formData.get('date'),
+          summary: formData.get('summary'),
+        };
+        addPost(newPost);
+        addForm.reset();
+      });
+    },
+  };
+})();
+
+// Initialize the module when the DOM is ready
+document.addEventListener('DOMContentLoaded', () => {
+  Blog.init();
+});
